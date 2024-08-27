61a40116dff079aee29239780405b6c30d916f9e
Ivan Shutko <ivan.shutko@intellias.com>
2024-08-27 22:27:49 +0300

    init

--- resources/views/estimation/form.blade.php
+++ resources/views/estimation/form.blade.php
@@ -1,30 +1,41 @@
 <form method="post" action="{{ route('calculate') }}">
     @csrf
 
-    <label for="class">{{ __('messages.class') }}</label>
-    <input type="text" id="class" name="class" value="{{ old('class', '10') }}">
-    @error('class')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
+    <div>
+        <label for="class">{{ __('messages.class') }}</label>
+        <input type="text" id="class" name="class" value="{{ old('class', '10') }}">
+        @error('class')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    </div>
 
-    <label for="methods">{{ __('messages.methods') }}</label>
-    <input type="text" id="methods" name="methods" value="{{ old('methods', '9.5') }}">
-    @error('methods')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
+    <div>
+        <label for="methods">{{ __('messages.methods') }}</label>
+        <input type="text" id="methods" name="methods" value="{{ old('methods', '9.5') }}">
+        @error('methods')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    </div>
 
-    <label for="dit">{{ __('messages.dit') }}</label>
-    <input type="text" id="dit" name="dit" value="{{ old('dit', '3') }}">
-    @error('dit')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
+    <div>
+        <label for="dit">{{ __('messages.dit') }}</label>
+        <input type="text" id="dit" name="dit" value="{{ old('dit', '3') }}">
+        @error('dit')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    </div>
 
-    <label for="confidence">{{ __('messages.confidence') }}</label>
-    <input type="text" id="confidence" name="confidence" value="{{ old('confidence', '0.95') }}">
-    @error('confidence')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    <button type="submit">{{ __('messages.submit') }}</button>
+    <div>
+        <label for="confidence">{{ __('messages.confidence') }}</label>
+        <input type="text" id="confidence" name="confidence" value="{{ old('confidence', '0.95') }}">
+        @error('confidence')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    </div>
+
+    <div>
+        <button type="submit">{{ __('messages.submit') }}</button>
+    </div>
 </form>
 
 <style>

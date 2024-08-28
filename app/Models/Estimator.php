1026c9c90a713010b1b04f7b987b5ffd61f59b09
Ivan Shutko <ivan.shutko@intellias.com>
2024-08-29 01:13:41 +0300

    init

--- app/Models/Estimator.php
+++ app/Models/Estimator.php
@@ -6,43 +6,59 @@ use Illuminate\Database\Eloquent\Model;
 use MathPHP\Probability\Distribution\Continuous\StudentT;
 use MathPHP\LinearAlgebra\MatrixFactory;
 
+/**
+ * @property mixed $class
+ * @property mixed $methods
+ * @property mixed $dit
+ * @property mixed $confidence
+ */
 class Estimator extends Model
 {
     protected $fillable = ['class', 'methods', 'dit', 'confidence'];
 
+    // Ці параметри визначають базовий рівень та вплив незалежних змінних на залежну змінну в рамках нелінійної регресійної моделі.
     private $intercept = -1.63782126445733;
     private $coeffs = [0.96641578624015, 0.96301298247654, -0.46272880601894];
+
+    // Розмір вибірки впливає на точність оцінок і ширину довірчих інтервалів
     private $N = 45;
+
+    // Стандартне відхилення остатків моделі, що вказує на точність моделі.
     private $S_Y = 0.0090855323014;
+
+    // Матриця коваріацій або кореляцій між перетвореними незалежними змінними,
+    // що допомагає оцінити ступінь лінійної залежності між ними.
     private $ZX = [
         [0.27127997, -0.04335353, 0.25063032],
         [-0.04335353, 1.53383441, -0.71641416],
         [0.25063032, -0.71641416, 10.63646880]
     ];
+
+    // Середні значення незалежних змінних після перетворення, використовувані для центрування даних перед аналізом.
     private $Z_MEAN = [1.4497370, 0.5378432, 0.1425785];
 
-    public function calculateEstimations()
+    public function calculateEstimations(): array
     {
         $metrics = [$this->class, $this->methods, $this->dit];
         $confLevel = $this->confidence;
 
-        // Создаем массив zx как массив массивов для подготовки к матричным операциям
+        // Створюємо масив zx як масив масивів для підготовки до матричних операцій
         $zx = array_map(function ($metric, $mean) {
             return [log10($metric) - $mean];
         }, $metrics, $this->Z_MEAN);
 
-        // Создаем матрицы для вычислений
+        // Створюємо матриці для обчислень
         $zxMatrix = MatrixFactory::create($zx);
         $ZXMatrix = MatrixFactory::create($this->ZX);
         $product = $zxMatrix->transpose()->multiply($ZXMatrix)->multiply($zxMatrix);
         $se_z = $product->get(0, 0);
 
-        // Расчет размера проекта
+        // Розрахунок розміру проекту
         $size = pow(10, $this->intercept) * pow($metrics[0], $this->coeffs[0]) * pow($metrics[1], $this->coeffs[1]) * pow($metrics[2], $this->coeffs[2]);
         $df = $this->N - 4;
         $t_value = $this->tDistribution($df, $confLevel);
 
-        // Расчет доверительного и предсказательного интервалов
+        // Розрахунок довірчого та передбачуваного інтервалів
         $conf_val = $t_value * sqrt($this->S_Y * (1.0 / $this->N + $se_z));
         $conf_interval = [
             pow(10, log10($size) - $conf_val),

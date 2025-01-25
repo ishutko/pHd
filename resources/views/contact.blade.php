af30fb689b9e834ab0396868cd96921e90d2a2ec
Ivan Shutko <ivan.shutko@intellias.com>
2025-01-25 22:24:24 +0200

    init

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <header>
+            <h1>Контакти</h1>
+        </header>
+        <main>
+            <section>
+                <p>Регресійні моделі для оцінювання кількості рядків коду веб-застосунків на основі фреймворків <strong>Сodeigniter</strong>, <strong>CakePhp</strong>, <strong>Yii</strong> за допомогою веб застосунку на фреймворку Laravel</p>
+            </section>
+            <section>
+                <h2>Автори</h2>
+                <p>
+                    <strong>С. Б. Приходько</strong><br>
+                    д.т.н., завідувач кафедри програмного забезпечення автоматизованих систем<br>
+                    <a href="https://orcid.org/0000-0002-2325-018X" target="_blank">https://orcid.org/0000-0002-2325-018X</a>
+                </p>
+                <p>
+                    <strong>І. С. Шутко</strong><br>
+                    аспірант кафедри програмного забезпечення автоматизованих систем<br>
+                    <a href="https://orcid.org/0000-0001-8584-113X" target="_blank">https://orcid.org/0000-0001-8584-113X</a>
+                </p>
+            </section>
+            <section>
+                <h2>Університет</h2>
+                <p>Національний університет кораблебудування імені адмірала Макарова<br>
+                    Проспект Героїв України, 9, м. Миколаїв, 54007</p>
+            </section>
+            <footer>
+                <p>© 2025 Контакти | Фреймворки: Yii, CakePHP, CodeIgniter</p>
+            </footer>
+        </main>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalculationController;
 
@@ -7,3 +8,5 @@ Route::get('/calculation', [CalculationController::class, 'showForm'])->name('ca
 Route::post('/calculation', [CalculationController::class, 'calculate'])->name('calculate');
 Route::get('/calculation/random', [CalculationController::class, 'generateRandom'])->name('generate-random');
 Route::get('/calculation/example-data', [CalculationController::class, 'getExampleData'])->name('get-example-data');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/about', [PageController::class, 'contact'])->name('contact');
